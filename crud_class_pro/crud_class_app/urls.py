3b5b4a225b235f847db233bd38abcfe45603529a
Bindu Bhatiya <bhatiyabindu81@gmail.com>
2024-09-12 16:30:35 +0530

    crud_class_based

--- crud_class_pro/crud_class_app/urls.py
+++ crud_class_pro/crud_class_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import View_Data,Add_Student,EditStudent,Delete_Student
-#from .views import View as crud_class_app_views
+#from .views import View_Data,Add_Student,EditStudent,Delete_Student
+from crud_class_app import views as core_views
+
 urlpatterns = [
-    path('', View_Data.as_view(), name='view-data'),
-    path('add/', Add_Student.as_view(), name='add'),
-    path('edit-student/<int:id>/', EditStudent.as_view(), name='edit-student'),
-    path('delete-student/<int:id>/', Delete_Student.as_view(), name='delete-student'),
+    path('', core_views.View_Data.as_view(), name='view-data'),
+    path('add/', core_views.Add_Student.as_view(), name='add'),
+    path('edit-student/<int:id>/', core_views.EditStudent.as_view(), name='edit-student'),
+    path('delete-student/<int:id>/', core_views.Delete_Student.as_view(), name='delete-student'),
 ]
